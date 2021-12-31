3c3632c93b143088c87ec99f24519c04b6b9948a
guilhermow <gui.crb0@gmail.com>
2021-12-31 18:10:45 -0300

    refactoring Player fields

---
+++ Scripts/Utils.gd
@@ -0,0 +1,6 @@
+const UP = Vector2(0, -1)
+var motion = Vector2(0,0)
+var animationScript = load("res://Scripts/Animations.gd")
+var movementScript = load("res://Scripts/Movement.gd")
+var animation = animationScript.new()
+var movement = movementScript.new()
