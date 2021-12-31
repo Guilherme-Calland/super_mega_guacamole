d230568374e017f46d48dc3f3ee42f4d764e8d6d
guilhermow <gui.crb0@gmail.com>
2021-12-31 18:09:22 -0300

    refactoring fields

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,25 +1,22 @@
 extends KinematicBody2D
 
-const UP = Vector2(0, -1)
 export var gravity = 10
 export var speed = 100
 export var jump_speed = 300
-var motion = Vector2(0,0)
-var animationScript = load("res://Scripts/Animations.gd")
-var movementScript = load("res://Scripts/Movement.gd")
-var animation = animationScript.new()
-var movement = movementScript.new()
+
+var u
+
+func _ready():
+	var utilsScript = load("res://Scripts/Utils.gd")
+	u = utilsScript.new()
 
 func _physics_process(delta):
 	move()
 	animate()
 	
 func move():
-	motion = movement.move(motion, speed, gravity, jump_speed, is_on_floor())
-	move_and_slide(motion, UP)
+	u.motion = u.movement.move(u.motion, speed, gravity, jump_speed, is_on_floor())
+	move_and_slide(u.motion, u.UP)
 	
 func animate():
-	animation.animate(is_on_floor(), $PlayerAnimatedSprite)
-	
-
-
+	u.animation.animate(is_on_floor(), $PlayerAnimatedSprite)
