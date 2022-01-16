36a68fed3467ddee2f160cbb604cef973130065d
guilhermow <gui.crb0@gmail.com>
2022-01-16 09:59:13 -0300

    jumpPad functionality

--- Scenes/Objects/JumpPad.gd
+++ Scenes/Objects/JumpPad.gd
@@ -3,4 +3,9 @@ extends Area2D
 export var boostForce = 650
 
 func _on_JumpPad_body_entered(body):
+	$JumpPadSprite.play("boost")
+	$Timer.start()
 	body.boost(boostForce)
+
+func _on_Timer_timeout():
+	$JumpPadSprite.play("idle")
