83cf60b8ea6c742faa4e41425175a574ee0dbf80
guilhermow <gui.crb0@gmail.com>
2022-01-16 13:12:21 -0300

    implementing spiked moving object

--- Scripts/MovingObject.gd
+++ Scripts/MovingObject.gd
@@ -1,27 +1,35 @@
 extends Node2D
 
 export var direction = "still"
-export var dislocationTime = 1
+export var dislocation = 100
+var dislocationCounter = 0
 export var waitTime = 2
 export var throwDirection = Vector2(-10, -50)
 
+func _ready():
+	$Timer.wait_time = waitTime
+
 func _process(delta):
 	if direction == "forward":
+		dislocationCounter += 1
 		position.x += 1
 	elif direction == "backward":
+		dislocationCounter += 1
 		position.x -= 1
-
+	if dislocationCounter == dislocation:
+		changeDirections()
+	
 func _on_Timer_timeout():
-	if direction == "still":
-		$Timer.wait_time = dislocationTime
-		direction = "backward"
-	elif direction == "backward":
-		$Timer.wait_time = dislocationTime
-		direction = "forward"
-	elif direction == "forward":
-		$Timer.wait_time = waitTime
+	direction = "backward"
+	
+func changeDirections():
+	dislocationCounter = 0
+	if direction == "forward":
 		direction = "still"
-	$Timer.start()
+		$Timer.start()
+	elif direction == "backward":
+		direction = "forward"
+		
 
 func _on_Spike_body_entered(body):
 	body.hurt(throwDirection)
